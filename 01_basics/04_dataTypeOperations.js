74ad704e315952da5f9866b6d18e3ad3b69a8b9f
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-15 18:02:33 +0530

    DataType Operations

---
+++ 01_basics/04_dataTypeOperations.js
@@ -0,0 +1,23 @@
+let num=3;
+let negNum=-num;
+//console.log(negNum);
+
+//basic arithmatic operations
+// console.log(2-2);//substraction
+// console.log(2+2);//addition
+// console.log(2*2);//multiply
+// console.log(2**2);//power
+// console.log(2/2);//modulus
+// console.log(2%2);//reminder
+
+let str1="Hello";
+let str2="Sayali";
+let str3= str1+str2;
+console.log(str3);
+
+console.log(1+"2");//12 concat
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32 because of toPrimitive standard 
+
+
+
