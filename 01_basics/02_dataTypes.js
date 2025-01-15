dd1ce43950a0eae2fb7ec0886f4757c9e6ff4c16
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-15 17:18:17 +0530

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3);//ReferenceError: alert is not defined
+//its only give popup as a 6 on browser ,we are using nodejs
+
+
+console.log(3+3);//; not madatory but used for code readability
+
+console.log("Sayu");
+
+//number =>2 to power of 53
+let age =23;
+
+//String="" ,''
+let name="Sayali";
+
+//boolean= true /false
+let isloggedIn=false;
+
+//null==object standalone value,console.log(typeof null); it give type as Object
+
+//undefined = when the value is not assigned,type undefined
+
+//Symbol=>used to marked as a unique
+
+//object
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+
+
