102d9573261929892a8d4cda218f7f924da340ba
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-15 17:13:47 +0530

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+console.log("Sayali");
+
+//block scoped,can't be modified
+const userId=2191
+
+//block scoped can be modified
+let userName="Sayali"
+
+//function scoped
+var userAddress="Kolhapur"
+
+//we can right variable like this but its not proper way
+designation="software engineer";
+
+//by using console.log
+console.log(userId);
+console.log([userId,userName,userAddress,designation]);
+
+//by using console.table
+console.table([userId,userName,userAddress,designation]);
+
