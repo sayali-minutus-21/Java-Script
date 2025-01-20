8df61a9a2a3bf177bec4840652639eeebb56cd6d
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-20 16:01:53 +0530

    Number & Maths

---
+++ 01_basics/08_nums_and_maths.js
@@ -0,0 +1,37 @@
+// const num=2191;
+// console.log(num);
+
+// const amt=new Number(2191);
+// console.log(amt);
+
+// console.log(amt.toString().length);
+// console.log(amt.toFixed(1));//2191.0
+
+// const balance=21.91;
+
+// console.log(balance.toPrecision(1));
+
+// const hundreads=1000000;
+
+// console.log(hundreads.toLocaleString('en-IN'));
+
+// Number.MAX_VALUE();
+
+//***********Math************ */
+
+// console.log(Math.PI);
+// console.log(Math.abs(-21));
+// console.log(Math.round(21.99991));
+// console.log(Math.ceil(4.20));round figure to upper value
+// console.log(Math.floor(4.20));;lower value
+// console.log(Math.min(1,2,3,4));
+// console.log(Math.max(1,2,3,4));
+
+// console.log(Math.random());
+
+// console.log(Math.random()*10+1);
+// console.log(Math.random()*2+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
