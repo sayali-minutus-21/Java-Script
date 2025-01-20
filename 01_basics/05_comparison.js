0cf5caf01347141b6ac990704c66182414bc92f0
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-20 11:59:05 +0530

    comparison operators

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//console.log("5">1);//for this comparison datatypes must be same
+
+// console.log(null>0);
+// console.log(null==0);//NaN false
+// console.log(null>=0);//true null is converted into 0 its give 0
+
+//console.log(undefined==0);//false
+
+//=== checks value strictly including datatypes
+console.log("2"===2);//false
+console.log("2"==2);//true ==only checks value after parsing not datatype
+
+
