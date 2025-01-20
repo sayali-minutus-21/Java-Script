5ebc43022669a5e62294a2743aff0863d6c9fe88
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-20 12:21:37 +0530

    Primitive & NonPrimitive

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+//Primitive
+//7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+// const num=100;
+
+// const decnum=100.4;
+// const isNum=true;
+// const temp=null;
+
+// const id=Symbol('21');
+// const anotherId=Symbol('21');
+
+// console.log(id ==anotherId);//false
+
+// const bigNum=123456789101112131415n;
+// console.log(typeof bigNum);//bigint
+
+
+//reference type (non-primitive)
+//Array,Objects,Function
+
+const languages=["Java","C","C++","JS"];//Array
+
+let obj={
+    name:"Sayali",
+    age:23,
+}//{} object
+
+const func=function(){
+    console.log("Hello World...!");
+    
+}
+console.log(typeof func);//function
+
+
