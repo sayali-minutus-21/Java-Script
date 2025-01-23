a49e8f9188b55efea80a90067e2f61a285f5736d
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-23 15:52:52 +0530

    array & methods of array

---
+++ 02_core_basics/01_array.js
@@ -0,0 +1,44 @@
+//Array
+
+const arr=[0,1,2,3];
+// console.log(arr[0]);
+
+// const lang=["java","c","c++"];
+
+// const myarr=new Array(1,2,3,4,5,6);
+// console.log(myarr[1]);
+
+// //Array Methods
+
+// arr.push(6);//used to add value to array
+// console.log(arr);//[ 0, 1, 2, 3, 6 ]
+
+// arr.pop();//removes last element from array
+// console.log(arr);//[ 6, 0, 1, 2, 3 ]
+
+
+// arr.unshift(6);//ading element to the start [ 6, 0, 1, 2, 3 ]
+// console.log(arr);//
+
+// console.log(arr.shift());//prints first element
+
+// console.log(arr.includes(2));// true  ,check the vaue is present in array or not gives boolean result
+// console.log(arr.indexOf(1));//1, gives the index of mentioned value
+
+// const newAr=arr.join();//joins two array and convert it into String
+
+// console.log(arr);//[ 0, 1, 2, 3 ]
+// console.log(newAr);//0,1,2,3
+// console.log(typeof newAr);//String
+
+//slice ,splice
+// console.log("A",arr);//A [ 0, 1, 2, 3 ]
+
+// const myar1=arr.slice(1,3);//only prints the values given index
+// console.log(myar1);//[1,2]
+// console.log("b",arr);//B [ 0, 1, 2, 3 ]
+
+// const myar2=arr.splice(1,3);//manupluates original array and removes the values till mentioned range
+// console.log(myar2);//[1,2,3]
+// console.log("c",arr);
+
