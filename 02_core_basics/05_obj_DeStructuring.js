c8410ecc9fc9bf8f66e5b90288d1149044ebf62f
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-24 11:52:01 +0530

    Object Destructuring

---
+++ 02_core_basics/05_obj_DeStructuring.js
@@ -0,0 +1,26 @@
+const course={
+    name:"JS",
+    price:900,
+    Instructor:"Sayali"
+}
+
+const {name}=course;
+console.log(name);
+
+
+//console.log(typeof nm);
+console.log(typeof name);
+console.log(typeof course);
+
+
+const {Instructor:inst}=course;
+console.log(inst);
+
+// const navbar=({company})=> {
+// console.log();
+
+// }
+ 
+// navbar(company="MinComp");
+
+//API
