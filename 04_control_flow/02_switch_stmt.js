5b8707959623d23a6d93a29b5ae200224e6659d6
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-27 17:30:06 +0530

    switch statement

---
+++ 04_control_flow/02_switch_stmt.js
@@ -0,0 +1,30 @@
+//switch
+
+const day="monday";
+switch (day) {
+    case "monday":
+        console.log("its monday");
+        break;
+    case "tuesday":
+        console.log("its tuesday");
+    default:
+        break;
+}
+
+const month=1
+switch (month) {
+    case 1:
+        console.log("its january");
+        break;
+    case 2:
+        console.log("its feb");
+        break;
+
+    case 3:
+        console.log("its march");
+        break;
+    default:
+        break;
+}
+//break is mandotory ,if we dont right break it will fall till default
+//all the cases will executed
