d26dd5417143249d9be45a6a8faa6c359e66606c
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-27 18:01:57 +0530

    truthy & falsy

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,33 @@
+// const usrmail="s@gmail.com";
+
+const usrmail='';//only check for value
+
+// if (usrmail) {
+//     console.log("got useremail");
+// } else {
+//     console.log("dont have user mail");
+// }
+
+//falsy value
+
+//false , 0 ,-0, BigInt 0n, "" , null , undefined , NaN
+//except this all values are truthy
+
+//truthy value
+//"0", 'false', " ",[],{}, function(){}
+
+// if (usrmail.length===0) {
+//     console.log("Array is empty");
+// }
+
+// const obj={};
+// if (Object.keys(obj).length===0) {
+//     console.log("obj is empty");
+// }
+
+//=>true
+//false==0 
+//false==''
+//0==''
+
+
