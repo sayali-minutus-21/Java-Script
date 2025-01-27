0c11515ecc6441bb5d92b77a69995e6d0188c732
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-27 16:41:13 +0530

    if else statement

---
+++ 04_control_flow/01_if_else_stmt.js
@@ -0,0 +1,79 @@
+//if
+
+if (true) {
+    //content from here will execute
+}
+if (false) {
+    //not executed
+}
+
+
+// if (2==2) {
+//     console.log("executed");
+    
+// }
+
+const isLoggedIn=true;
+const temp=29;
+
+// if (temp<50) {
+//     console.log("temp is less than 50");
+// }
+// // if (isLoggedIn) {
+    
+// // }
+// if (2!=3) {
+//     console.log("executed");
+// }
+//2==="2"
+//2<=2 (true)==> <,>, <= ,>= ,== ,!= ,===(checks type also)
+
+//if else
+// if (temp>25 ) {
+//     console.log("greater than 25");
+    
+// } else {
+//     console.log("not grater than 25");
+    
+// }
+
+
+//scope related
+const score=200;
+// if (score>100) {
+//     let power="fly";
+//     console.log(`user power : ${power}`);
+    
+// }
+// console.log(`user power : ${power}`);//due to global scope of var 
+
+//short hand notetion
+// if (score>100) console.log("test"),console.log("one");
+// ;
+//implicit scope only for single line ,can't right multiple code , but can right multiple line using , coma ,,but its not right way(unreadable code)
+
+// if (score<300) {
+//     console.log("less than 300");
+    
+// } else if(score<500){
+//     console.log("less than 500");
+// }
+// else if (score <100) {
+//     console.log("less than 100");
+// }
+
+//for multiiple condition usig 
+// && for both conditions are true
+// || for either one will true
+// const logIn=true;
+// const debited=true;
+// const loginFromGoogle=false;
+// const logginfromEmail=true;
+
+// if (logIn && debited) {
+//     console.log("allow to by phone");
+// }
+// if (loginFromGoogle || logginfromEmail) {
+//     console.log("user logged in"); 
+// }
+
