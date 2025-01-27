10211b05db26e55c1a4b43d37bb16c2fd0e8615d
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-27 12:54:16 +0530

    immediately invoked function expression

---
+++ 03_functions/iIFe.js
@@ -0,0 +1,19 @@
+//immediately Invoked Function Expression(IIFe)
+
+(function test() {
+    console.log(`Db connected`);
+})();//;used to end iife
+
+//()();//we use iife to avoid global scope pollution
+//but it dont know where to stop so we need to end this execution
+
+//named iife
+(function test(){
+    console.log(`from test`);
+})();
+
+//passing prameters
+((name) => {
+    console.log(`db 2 connected ${name}`);
+})('sayali');
+//when we want to give parameters
