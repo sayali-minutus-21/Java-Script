5b2cba693fb2fcdc64aaffb76327bbd6406eedb5
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-28 10:49:36 +0530

    while loop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,16 @@
+//while loop
+
+let i=0;
+// while (i<=10) {
+//     console.log(`value of i is ${i}`);
+//     i=i+2;
+// }
+
+
+let arr=['java','c++','js'];
+
+let narr=0;
+// while (narr<arr.length) {
+//    console.log(`value is ${arr[narr]}`);
+//     narr=narr+1;
+// }
