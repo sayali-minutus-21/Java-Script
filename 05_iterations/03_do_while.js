fc6f54a5124a3f41309d0db9d7e5b5c8ae884ae7
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-01-28 10:49:55 +0530

    do while loop

---
+++ 05_iterations/03_do_while.js
@@ -0,0 +1,17 @@
+//do while loop
+//minimal execution is 1
+//first execute the statement and then check the condition
+
+// let score=1;
+// do {
+//     console.log(`score is ${score}`);
+//     score++;
+// } while (score<=10);
+
+
+let score=11;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
+//11 is printed and it check for condition ,condition get false and it stop the iteration 
